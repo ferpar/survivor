d1b125a28f1cf42690cd2de31845d09597eacbe2
fer <ferp@protonmail.ch>
2023-05-18 11:17:44 +0200

    fix datapoints being sent as strings instead of numbers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,14 +1,16 @@
 import { RawDataPoint } from "../types/domain";
 
+// parseFloat avoiding exponential notation
+
 export function coinAPIToArray(marketData: any): RawDataPoint[] {
   const adaptedMarketData = marketData.map((datapoint: any) => {
     return [
       new Date(datapoint.time_period_start).getTime(),
-      datapoint.price_open,
-      datapoint.price_high,
-      datapoint.price_low,
-      datapoint.price_close,
-      datapoint.volume_traded,
+      parseFloat(datapoint.price_open),
+      parseFloat(datapoint.price_high),
+      parseFloat(datapoint.price_low),
+      parseFloat(datapoint.price_close),
+      parseFloat(datapoint.volume_traded),
     ];
   });
   return adaptedMarketData;
